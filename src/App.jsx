14e336475ef21b1f491ac405b9d25bd369184187
ShrinivasDhole <shrinivasdhole7@gmail.com>
2024-02-13 19:08:57 +0530

    tailwindBasic

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import { RevenueCard } from './components/RevenueCard';
+function App() {
+
+  return (
+    <div className='grid grid-cols-3'>
+      <RevenueCard title={"Amount Pending"}  amount={"92,312.20"} orderCount={13} > </RevenueCard>
+    </div>
+  )
+}
+
+export default App
